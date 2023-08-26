ba512affc41721ef2c4d08c5327e27462419d7e5
Francisco Jose Marquina Garcia <aldeadavila@gmail.com>
2023-08-26 15:43:44 +0200

    66 Actualizar categoría

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -6,6 +6,7 @@ import { JwtRole } from 'src/auth/jwt/jwt-role';
 import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
 import { JwtRolesGuard } from 'src/auth/jwt/jwt-roles-guard';
 import { CreateCategoryDto } from './dto/create.category.dto';
+import { UpdateCategoryDto } from './dto/update.category.dto';
 
 @Controller('categories')
 export class CategoriesController {
@@ -23,7 +24,7 @@ export class CategoriesController {
     @UseGuards(JwtAuthGuard, JwtRolesGuard)
     @Post() // http://localhost:3000/categories
     @UseInterceptors(FileInterceptor('file'))
-    uploadWithImage(
+    createWithImage(
         @UploadedFile(
             new ParseFilePipe({
                 validators: [
@@ -36,6 +37,34 @@ export class CategoriesController {
         @Body() category: CreateCategoryDto
         ) {
         return this.categoriesService.create(file, category)
+    }
+
+    @HasRoles(JwtRole.ADMIN)
+    @UseGuards(JwtAuthGuard, JwtRolesGuard)
+    @Put(':id')
+    update(
+        @Param('id', ParseIntPipe) id: number, @Body() category: UpdateCategoryDto 
+    ) {
+        return this.categoriesService.update(id, category)
+    }
+
+    @HasRoles(JwtRole.ADMIN)
+    @UseGuards(JwtAuthGuard, JwtRolesGuard)
+    @Put('upload/:id') // http://localhost:3000/categories
+    @UseInterceptors(FileInterceptor('file'))
+    updateWithImage(
+        @UploadedFile(
+            new ParseFilePipe({
+                validators: [
+                  new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 2 }),
+                  new FileTypeValidator({ fileType: '(png|jpeg|jpg)' }),
+                ],
+              }),
+        ) file: Express.Multer.File,
+        @Param('id', ParseIntPipe) id: number,
+        @Body() category: UpdateCategoryDto
+        ) {
+        return this.categoriesService.updateWithImage(file, id, category)
     }
 
 
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -4,6 +4,7 @@ import storage = require('../utils/cloud_storage')
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Category } from './category.entity';
+import { UpdateCategoryDto } from './dto/update.category.dto';
 
 @Injectable()
 export class CategoriesService {
@@ -30,5 +31,40 @@ export class CategoriesService {
         return this.categoriesRepository.save(category)
 
     }
+
+    async update(id:number, category: UpdateCategoryDto) {
+     
+        const categoryFound = await this.categoriesRepository.findOneBy({id: id});
+
+        if (!categoryFound) {
+            throw new HttpException('La categoría no existe', HttpStatus.NOT_FOUND);
+        }
+        const updateCategory = Object.assign(categoryFound, category);
+        
+        return this.categoriesRepository.save(updateCategory)
+
+    }
+
+    async updateWithImage(file: Express.Multer.File, id:number, category: UpdateCategoryDto) {
+        
+        
+        const url = await storage(file, file.originalname);
+        if (url === undefined && url === null) {
+            throw new HttpException('La imagen no se pudo guardar', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        const categoryFound = await this.categoriesRepository.findOneBy({id: id});
+
+        if (!categoryFound) {
+            throw new HttpException('La categoría no existe', HttpStatus.NOT_FOUND);
+        }
+
+        category.image = url
+        const updateCategory = Object.assign(categoryFound, category);
+        
+        return this.categoriesRepository.save(updateCategory)
+
+    }
+
+   
 
 }
