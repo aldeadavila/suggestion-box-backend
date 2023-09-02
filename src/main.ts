f236ab07f71264178347068f58f31edeb31a5b00
Francisco Jose Marquina Garcia <aldeadavila@gmail.com>
2023-09-02 12:03:48 +0200

    config port

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+// Use `PORT` provided in environment or default to 3000
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({forbidUnknownValues: false}));
-  await app.listen(process.env.PORT, '0.0.0.0');
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
