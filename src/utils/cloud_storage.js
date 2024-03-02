23edcf7f8d55c12bb7ab601cbba55955283f3d92
Francisco Jose Marquina Garcia <aldeadavila@gmail.com>
2024-03-02 15:55:36 +0100

    continuing testing

--- src/utils/cloud_storage.js
+++ src/utils/cloud_storage.js
@@ -10,7 +10,7 @@ const { ConfigService } = require('@nestjs/config');
 
 const storage = new Storage({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    keyFilename: process.env.FIREBASE_CONFIG
+    keyFilename: JSON.stringify(process.env.FIREBASE_CONFIG)
 });
 
 const bucket = storage.bucket("gs://suggestion-box-19f10.appspot.com/");
