2ddbd01cb4e9489fb2bd3ac1e741ad363838e010
Francisco Jose Marquina Garcia <aldeadavila@gmail.com>
2024-03-02 15:13:45 +0100

    change keyfilename

--- src/utils/cloud_storage.js
+++ src/utils/cloud_storage.js
@@ -11,19 +11,7 @@ const { ConfigService } = require('@nestjs/config');
 const project_id = ConfigService.get<String>('firebase.FIREBASE_PROJECT_ID');
 const storage = new Storage({
     projectId: project_id,
-    keyFilename: {
-        "type": ConfigService.get<String>('firebase.FIREBASE_TYPE'),
-        "project_id": project_id,
-        "private_key_id": ConfigService.get<String>('firebase.FIREBASE_PROJECT_ID'),
-        "private_key": ConfigService.get<String>('firebase.FIREBASE_PRIVATE_KEY'),
-        "client_email": ConfigService.get<String>('firebase.FIREBASE_CLIENT_EMAIL'),
-        "client_id": ConfigService.get<String>('firebase.FIREBASE_CLIENT_ID'),
-        "auth_uri": ConfigService.get<String>('firebase.FIREBASE_AUTH_URI'),
-        "token_uri": ConfigService.get<String>('firebase.FIREBASE_TOKEN_URI'),
-        "auth_provider_x509_cert_url": ConfigService.get<String>('firebase.FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
-        "client_x509_cert_url": ConfigService.get<String>('firebase.FIREBASE_CLIENT_X509_CERT_URL'),
-        "universe_domain": ConfigService.get<String>('firebase.FIREBASE_UNIVERSE_DOMAIN'),
-      }
+    keyFilename: process.env.FIREBASE_CONFIG
 });
 
 const bucket = storage.bucket("gs://suggestion-box-19f10.appspot.com/");
